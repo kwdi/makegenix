14e447ba8e6e04f83ba3a98989e2d28d89b956a4
Konstantinos <kostas.droid@gmail.com>
2021-01-14 12:45:15 +0100

    shop controller write to db

--- controllers/shops.js
+++ controllers/shops.js
@@ -1,36 +1,81 @@
+const Shop = require('../models/Shop')
+
 
 // @desc Get all shops
 // @route GET /api/v1/shops
 // @access Public
-exports.getShops = (req, res, next ) => {
-    res.status(200).json({success: true, msg: 'Show all shops'});
+exports.getShops = async (req, res, next ) => {
+    
+    const shops = await Shop.find();
+
+
+    res.status(200).json({success: true, count: shops.length , data: shops});
+
+    // res.status(400).json({success: false})
 }
 
 
 // @desc Get single shop
 // @route GET /api/v1/shops/:id
 // @access Public
-exports.getShop = (req, res, next ) => {
-    res.status(200).json({success: true, msg: `Show shop ${req.params.id}` });
+exports.getShop = async (req, res, next ) => {
+
+    const shop = await Shop.findById(req.params.id);
+
+    if(!shop){
+       return  res.status(400).json({success: false})
+    }
+    res.status(200).json({success: true, data: shop });
+
+    //res.status(400).json({success: false})
 }
 
 // @desc Create new shop
 // @route POST /api/v1/shops/:id
 // @access Private
-exports.createShop = (req, res, next ) => {
-    res.status(200).json({success: true, msg: 'Create new shop'});
+exports.createShop = async (req, res, next ) => {
+    try{
+        const shop = await Shop.create(req.body);
+    
+    res.status(201).json({
+        success: true,
+        data: shop
+    })
+    }catch(err) {
+        res.status(400).json({success: false})
+    }  
 }
 
 // @desc Update shop
 // @route PUT /api/v1/shops/:id
 // @access Private
-exports.updateShop = (req, res, next ) => {
-    res.status(200).json({success: true, msg: `Update shop ${req.params.id}` });
+exports.updateShop = async (req, res, next ) => {
+
+    const shop = await Shop.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    if(!shop){
+        return  res.status(400).json({success: false})
+     }
+
+    res.status(200).json({success: true, data: shop });
+
+    //res.status(400).json({success: false})
 }
 
 // @desc Delete single shop
 // @route DELETE /api/v1/shops/:id
 // @access Private
-exports.deleteShop = (req, res, next ) => {
-    res.status(200).json({success: true, msg: `Delete shop ${req.params.id}` });
+exports.deleteShop = async (req, res, next ) => {
+    
+    const shop = await Shop.findByIdAndDelete(req.params.id)
+    
+    if(!shop){
+        return  res.status(400).json({success: false})
+     }
+
+
+    res.status(200).json({success: true, data: {} });
 }
