4c5bf74073a88d8c190ca8b064bee565fd08b4d2
Konstantinos <kostas.droid@gmail.com>
2021-01-14 22:01:48 +0100

    product CRUD

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require("../utils/errorResponse");
+const Store = require('../models/Store');
 
 
 // @desc Get products
@@ -24,4 +25,83 @@ exports.getProducts = asyncHandler(async (req, res, next) =>{
     const products = await query;
       
     res.status(200).json({ success: true, count: products.length, data: products})
+});
+
+// @desc Get products
+// @route GET /api/v1/product/:id
+// @access Public
+
+exports.getProduct = asyncHandler(async (req, res, next) =>{
+    
+    const product = await Product.findById(req.params.id).populate({ 
+        path: 'store', 
+        select: 'name description'
+    });
+
+    if(!Product){
+        return next(new ErrorResponse(`Product not found with id of ${req.params.id}`, 404))
+    }
+      
+    res.status(200).json({ success: true, data: product})
 })
+
+// @desc Add products
+// @route GET /api/v1/stores/:storeId/products
+// @access Private
+
+exports.addProduct = asyncHandler(async (req, res, next) =>{
+    
+    req.body.store = req.params.storeId;
+
+    const store = await Store.findById(req.params.storeId);
+
+    if(!store){
+        return next(new ErrorResponse(`Product not found with id of ${req.params.storeid}`, 404))
+    }
+      
+    const product = await Product.create(req.body);
+
+
+    res.status(200).json({ success: true, data: product})
+})
+
+// @desc Update product
+// @route PUT /api/v1/products/:id
+// @access Private
+
+exports.updateProduct = asyncHandler(async (req, res, next) =>{
+    
+
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorResponse(`Product not found with id of ${req.params.id}`, 404))
+    }
+      
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({ success: true, data: product})
+
+});
+
+// @desc Delete product
+// @route DELETE /api/v1/products/:id
+// @access Private
+
+exports.deleteProduct = asyncHandler(async (req, res, next) =>{
+    
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorResponse(`Product not found with id of ${req.params.id}`, 404))
+    }
+      
+    await product.remove();
+
+    res.status(200).json({ success: true, data: {}})
+
+});
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const { getProducts } = require('../controllers/products');
+const { 
+    getProducts, 
+    getProduct, 
+    addProduct, 
+    updateProduct,
+    deleteProduct} = require('../controllers/products');
 
 const router = express.Router({mergeParams: true});
 
-router.route('/').get(getProducts);
+router
+    .route('/')
+    .get(getProducts)
+    .post(addProduct);
+
+
+router
+    .route('/:id')
+    .get(getProduct)
+    .put(updateProduct)
+    .delete(deleteProduct);
 
 module.exports = router;
