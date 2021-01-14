0a83ca58867089d0b0d79baf10ce6c7633eb66b0
Konstantinos <kostas.droid@gmail.com>
2021-01-14 11:43:31 +0100

    shop schema v1

---
+++ models/shop.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const ShopSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    slug: String,
+    website: {
+        type: String,
+        match: [
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+          'Please use a valid URL with HTTP or HTTPS'
+        ]
+      },
+      phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+      },
+      email: {
+        type: String,
+        match: [
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+          'Please add a valid email'
+        ]
+      },
+      address: {
+        type: String,
+        required: [true, 'Please add an address']
+      },
+      location: {
+        // GeoJSON Point
+        type: {
+          type: String,
+          enum: ['Point']
+        },
+        coordinates: {
+          type: [Number],
+          index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
+      },
+      products: {
+        // Array of strings
+        type: [String],
+        required: true,
+        enum: [
+          'Electronics',
+          'Home and Garden',
+          'Fashion',
+          'Sports',
+          'Toys',
+          'Other'
+        ]
+      },
+      photo: {
+        type: String,
+        default: 'no-photo.jpg'
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+
+module.exports = mongoose.model('Shop', ShopSchema);
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Router
-
-const shops = require('./routes/shops');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
+// Router
+const shops = require('./routes/shops');
+
+
+
 const app = express();
+
+// Dev logger middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 
 // Mouter routers
 app.use('/api/v1/shops', shops);
@@ -16,4 +29,16 @@ app.use('/api/v1/shops', shops);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+
+    );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close (() => process.exit(1));
+})
+
