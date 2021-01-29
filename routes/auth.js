deb17b65b6435d8a234c32195872521d9549edfc
Konstantinos <kostas.droid@gmail.com>
2021-01-29 02:30:20 +0100

    admin crud

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword  } = require('../controllers/auth');
+const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword, logout  } = require('../controllers/auth');
 
 const router = express.Router();
 const { protect } = require('../middleware/auth');
@@ -8,6 +8,7 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.get('/logout', logout);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword) ;
 router.post('/forgotpassword', forgotPassword);
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ connectDB();
 const stores = require('./routes/stores');
 const products = require('./routes/products');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -41,6 +42,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/stores', stores);
 app.use('/api/v1/products', products);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+
 
 app.use(errorHandler);
 
